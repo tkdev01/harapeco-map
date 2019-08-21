da80bcb8f51d9697bdae8a736f5989e273a6f25b
Oyama Takuro <oyama@MacBookPro.local>
2019-08-21 15:48:27 +0900

    Map表示のPCスマホ対応

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,13 @@
         <!-- <logo />
         <vuetify-logo /> -->
       </div>
-      <iframe id="googleMap" src="https://www.google.com/maps/d/embed?mid=1efoj3MYRdsJb7c5cfSwf8OXcMtIf6K1d" width="100%" height="480"></iframe>
+      <!-- MyMapの埋め込み -->
+      <div v-if="$vuetify.breakpoint.xs">
+        <iframe id="googleMap" src="https://www.google.com/maps/d/embed?mid=1efoj3MYRdsJb7c5cfSwf8OXcMtIf6K1d" width="100%" height="480"></iframe>
+      </div>
+      <div v-else>
+        <iframe id="googleMap" src="https://www.google.com/maps/d/embed?mid=1efoj3MYRdsJb7c5cfSwf8OXcMtIf6K1d" width="700" height="550"></iframe>
+      </div>
       <!-- <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
